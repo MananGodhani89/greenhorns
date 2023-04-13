78495fff36e8627f699acd3790b9de8ed2008c89
MananGodhani89 <130462891+MananGodhani89@users.noreply.github.com>
2023-04-13 22:28:13 +0530

    Update greenhorns1.c

--- greenhorns1.c
+++ greenhorns1.c
@@ -18,5 +18,8 @@ main()
 	printf("*   *   *   *   *   *   *   *\n");
 	printf("*\t* \t\t    *\n");
 	printf("*\t* \t*   *   *   *\n");
+	printf("*\t* \t* \t*   *\n");
+	printf("*   *   *   *   *   *   *   *\n");
+	
 	
 }
